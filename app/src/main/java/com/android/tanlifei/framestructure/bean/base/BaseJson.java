84fd78cf4e693f9f6d3618191fd0a9c1b720fa01
vic-tan <179840045@qq.com>
2015-09-06 16:20:00 +0800

    我的初始项目....

--- app/src/main/java/com/android/tanlifei/framestructure/bean/base/BaseJson.java
+++ app/src/main/java/com/android/tanlifei/framestructure/bean/base/BaseJson.java
@@ -5,52 +5,45 @@ package com.android.tanlifei.framestructure.bean.base;
  */
 public class BaseJson {
 
-	private String code;
-	private String msg;
-	private String data;
-
-	
-
-	public String getCode() {
-		return code;
-	}
+    private String code;
+    private String msg;
+    private String data;
 
 
-
-	public void setCode(String code) {
-		this.code = code;
-	}
-
+    public String getCode() {
+        return code;
+    }
 
 
-	public String getMsg() {
-		return msg;
-	}
+    public void setCode(String code) {
+        this.code = code;
+    }
 
 
-
-	public void setMsg(String msg) {
-		this.msg = msg;
-	}
-
+    public String getMsg() {
+        return msg;
+    }
 
 
-	public String getData() {
-		return data;
-	}
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
 
 
-
-	public void setData(String data) {
-		this.data = data;
-	}
-
+    public String getData() {
+        return data;
+    }
 
 
-	@Override
-	public String toString() {
-		return "HandlerJsonMsg [code=" + code + ", msg=" + msg + ", data="
-				+ data + "]";
-	}
+    public void setData(String data) {
+        this.data = data;
+    }
+
+
+    @Override
+    public String toString() {
+        return "HandlerJsonMsg [code=" + code + ", msg=" + msg + ", data="
+                + data + "]";
+    }
 
 }
