422a70034168c4652f0b8c3bbb21f97da6f2b616
vic-tan <179840045@qq.com>
2015-09-18 15:10:34 +0800

    把hanlder 返回改成接口回调的方式，因为handler 会存在内存泄漏情况。。 。。。。

---
+++ app/src/main/java/com/android/tanlifei/framestructure/common/http/base/RequesBean.java
@@ -0,0 +1,36 @@
+package com.android.tanlifei.framestructure.common.http.base;
+
+import com.android.tanlifei.framestructure.common.utils.MapUtils;
+import com.android.tanlifei.framestructure.common.utils.StringUtils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 请求网络接口参数封装类
+ * Created by tanlifei on 15/9/18.
+ */
+public class RequesBean {
+
+    private final String url;//请求的url
+    private final Map<String, Object> requestParams;//返回内容
+    private final Map<String, Object> callbackParams;//多请求时区分任务标识
+
+    public RequesBean(String url, Map<String, Object> requestParams, Map<String, Object> backParams) {
+        this.callbackParams = MapUtils.isEmpty(backParams) ? new HashMap<String, Object>() : backParams;
+        this.url = StringUtils.isEmpty(url) ? "" : url;
+        this.requestParams = MapUtils.isEmpty(requestParams) ? new HashMap<String, Object>() : requestParams;
+    }
+
+    public Map<String, Object> getCallbackParams() {
+        return callbackParams;
+    }
+
+    public Map<String, Object> getRequestParams() {
+        return requestParams;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+}
