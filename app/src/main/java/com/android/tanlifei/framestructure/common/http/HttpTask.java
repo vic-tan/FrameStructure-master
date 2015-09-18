4e1e4c56ce9607d609c81962047c05eecce5e3ee
vic-tan <179840045@qq.com>
2015-09-18 15:12:23 +0800

    把hanlder 返回改成接口回调的方式，因为handler 会存在内存泄漏情况。。 。。。。

--- app/src/main/java/com/android/tanlifei/framestructure/common/http/HttpTask.java
+++ app/src/main/java/com/android/tanlifei/framestructure/common/http/HttpTask.java
@@ -1,7 +1,7 @@
 package com.android.tanlifei.framestructure.common.http;
 
 import com.android.tanlifei.framestructure.common.http.base.BaseHttpTask;
-import com.android.tanlifei.framestructure.common.http.base.RequestBean;
+import com.android.tanlifei.framestructure.common.http.base.TaskParamsBean;
 import com.android.tanlifei.framestructure.common.utils.HttpUtils;
 import com.android.tanlifei.framestructure.engine.interf.IHttpTaskCallBack;
 
@@ -10,14 +10,14 @@ import com.android.tanlifei.framestructure.engine.interf.IHttpTaskCallBack;
  * 请求接口任务过程
  * <ul>
  * <strong>基本方法及自己方法</strong>
- * <li>{@link #get(RequestBean, IHttpTaskCallBack)}  get 请求 以普通形式提交参数</li>
- * <li>{@link #get(RequestBean, int, IHttpTaskCallBack)}  get 请求 以普通形式提交参数</li>
- * <li>{@link #getByJsonParams(RequestBean, IHttpTaskCallBack)}  get 请求 以json格式提交参数</li>
- * <li>{@link #getByJsonParams(RequestBean, int, IHttpTaskCallBack)}  get 请求 以json格式提交参数</li>
- * <li>{@link #post(RequestBean, int, IHttpTaskCallBack)}   post 请求，以普通形式提交参数</li>
- * <li>{@link #post(RequestBean, IHttpTaskCallBack)}   post 请求，以普通形式提交参数</li>
- * <li>{@link #postByJsonParams(RequestBean, int, IHttpTaskCallBack)}  post 请求，以json格式提交参数</li>
- * <li>{@link #postByJsonParams(RequestBean, IHttpTaskCallBack)}  post 请求，以json格式提交参数</li>
+ * <li>{@link #get(TaskParamsBean, IHttpTaskCallBack)}  get 请求 以普通形式提交参数</li>
+ * <li>{@link #get(TaskParamsBean, int, IHttpTaskCallBack)}  get 请求 以普通形式提交参数</li>
+ * <li>{@link #getByJsonParams(TaskParamsBean, IHttpTaskCallBack)}  get 请求 以json格式提交参数</li>
+ * <li>{@link #getByJsonParams(TaskParamsBean, int, IHttpTaskCallBack)}  get 请求 以json格式提交参数</li>
+ * <li>{@link #post(TaskParamsBean, int, IHttpTaskCallBack)}   post 请求，以普通形式提交参数</li>
+ * <li>{@link #post(TaskParamsBean, IHttpTaskCallBack)}   post 请求，以普通形式提交参数</li>
+ * <li>{@link #postByJsonParams(TaskParamsBean, int, IHttpTaskCallBack)}  post 请求，以json格式提交参数</li>
+ * <li>{@link #postByJsonParams(TaskParamsBean, IHttpTaskCallBack)}  post 请求，以json格式提交参数</li>
  * </ul>
  *
  * @author tanlifei
@@ -33,7 +33,7 @@ public class HttpTask extends BaseHttpTask {
      * @param callBackMethod 回调方法
      * @param callBackTag    多个请求任务区分标识
      */
-    public static void get(RequestBean params, int callBackTag, IHttpTaskCallBack callBackMethod) {
+    public static void get(TaskParamsBean params, int callBackTag, IHttpTaskCallBack callBackMethod) {
         if (!networkError(callBackMethod, callBackTag, params.getCallbackParams())) {
             HttpUtils.get(params, setAsyncHttpResponseHandler(params, callBackTag, callBackMethod));
         }
@@ -46,7 +46,7 @@ public class HttpTask extends BaseHttpTask {
      * @param params         请求参数实体
      * @param callBackMethod 回调方法
      */
-    public static void get(RequestBean params, IHttpTaskCallBack callBackMethod) {
+    public static void get(TaskParamsBean params, IHttpTaskCallBack callBackMethod) {
         if (!networkError(callBackMethod, -1, params.getCallbackParams())) {
             HttpUtils.get(params, setAsyncHttpResponseHandler(params, -1, callBackMethod));
         }
@@ -61,7 +61,7 @@ public class HttpTask extends BaseHttpTask {
      * @param callBackMethod 回调方法
      * @param callBackTag    多个请求任务区分标识
      */
-    public static void getByJsonParams(RequestBean params, int callBackTag,
+    public static void getByJsonParams(TaskParamsBean params, int callBackTag,
                                        IHttpTaskCallBack callBackMethod) {
         if (!networkError(callBackMethod, callBackTag, params.getCallbackParams())) {
             HttpUtils.getJsonParams(params, setAsyncHttpResponseHandler(params, callBackTag, callBackMethod));
@@ -74,7 +74,7 @@ public class HttpTask extends BaseHttpTask {
      * @param params         请求参数实体
      * @param callBackMethod 回调方法
      */
-    public static void getByJsonParams(RequestBean params,
+    public static void getByJsonParams(TaskParamsBean params,
                                        IHttpTaskCallBack callBackMethod) {
         if (!networkError(callBackMethod, -1, params.getCallbackParams())) {
             HttpUtils.getJsonParams(params, setAsyncHttpResponseHandler(params, -1, callBackMethod));
@@ -88,7 +88,7 @@ public class HttpTask extends BaseHttpTask {
      * @param callBackMethod 回调方法
      * @param callBackTag    多个请求任务区分标识
      */
-    public static void post(RequestBean params, int callBackTag,
+    public static void post(TaskParamsBean params, int callBackTag,
                             IHttpTaskCallBack callBackMethod) {
         if (!networkError(callBackMethod, callBackTag, params.getCallbackParams())) {
             HttpUtils.post(params, setAsyncHttpResponseHandler(params, callBackTag, callBackMethod));
@@ -101,7 +101,7 @@ public class HttpTask extends BaseHttpTask {
      * @param params         请求参数实体
      * @param callBackMethod 回调方法
      */
-    public static void post(RequestBean params,
+    public static void post(TaskParamsBean params,
                             IHttpTaskCallBack callBackMethod) {
         if (!networkError(callBackMethod, -1, params.getCallbackParams())) {
             HttpUtils.post(params, setAsyncHttpResponseHandler(params, -1, callBackMethod));
@@ -115,7 +115,7 @@ public class HttpTask extends BaseHttpTask {
      * @param callBackMethod 回调方法
      * @param callBackTag    多个请求任务区分标识
      */
-    public static void postByJsonParams(RequestBean params, int callBackTag,
+    public static void postByJsonParams(TaskParamsBean params, int callBackTag,
                                         IHttpTaskCallBack callBackMethod) {
         if (!networkError(callBackMethod, callBackTag, params.getCallbackParams())) {
             HttpUtils.postJsonParams(params, setAsyncHttpResponseHandler(params, callBackTag, callBackMethod));
@@ -128,7 +128,7 @@ public class HttpTask extends BaseHttpTask {
      * @param params         请求参数实体
      * @param callBackMethod 回调方法
      */
-    public static void postByJsonParams(RequestBean params,
+    public static void postByJsonParams(TaskParamsBean params,
                                         IHttpTaskCallBack callBackMethod) {
         if (!networkError(callBackMethod, -1, params.getCallbackParams())) {
             HttpUtils.postJsonParams(params, setAsyncHttpResponseHandler(params, -1, callBackMethod));
--- app/src/main/java/com/android/tanlifei/framestructure/common/http/base/TaskParamsBean.java
+++ app/src/main/java/com/android/tanlifei/framestructure/common/http/base/TaskParamsBean.java
@@ -10,13 +10,13 @@ import java.util.Map;
  * 请求网络接口参数封装类
  * Created by tanlifei on 15/9/18.
  */
-public class RequesBean {
+public class TaskParamsBean {
 
     private final String url;//请求的url
     private final Map<String, Object> requestParams;//返回内容
     private final Map<String, Object> callbackParams;//多请求时区分任务标识
 
-    public RequesBean(String url, Map<String, Object> requestParams, Map<String, Object> backParams) {
+    public TaskParamsBean(String url, Map<String, Object> requestParams, Map<String, Object> backParams) {
         this.callbackParams = MapUtils.isEmpty(backParams) ? new HashMap<String, Object>() : backParams;
         this.url = StringUtils.isEmpty(url) ? "" : url;
         this.requestParams = MapUtils.isEmpty(requestParams) ? new HashMap<String, Object>() : requestParams;
